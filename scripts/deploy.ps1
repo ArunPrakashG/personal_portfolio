96797461e80ac518f5435eb69dfedbb7e5457963
Arun Prakash <arun.prakash.456789@gmail.com>
2024-02-18 23:59:20 +0530

    Update Flutter SDK version and remove unused dependency

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,3 +1,3 @@
-& flutter pub global run peanut -b gh-pages | Out-Null
+& flutter pub global run peanut -b gh-pages --web-renderer canvaskit | Out-Null
 
 & git push origin --set-upstream gh-pages | Out-Null
